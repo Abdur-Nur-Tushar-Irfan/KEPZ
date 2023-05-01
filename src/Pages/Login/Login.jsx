a5902669a439f44ff482ee6bdf88694281926a84
unknown <abdurnur.tushar@youngonectg.com>
2023-05-01 16:39:22 +0600

    login added

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+const Login = () => {
+    return (
+        <div className='h-[400px] flex justify-center items-center'>
+            <div>
+               <h1>This is login pages</h1>
+               <form>
+                   
+               </form>
+
+            </div>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/firebase/firebase.init.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCJ0MMCFIHdImLIELBe2NRXuVBvc3cRNC0",
+  authDomain: "kepz-a361b.firebaseapp.com",
+  projectId: "kepz-a361b",
+  storageBucket: "kepz-a361b.appspot.com",
+  messagingSenderId: "959209922457",
+  appId: "1:959209922457:web:cf764e6ff55c5a2a62bd08"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
